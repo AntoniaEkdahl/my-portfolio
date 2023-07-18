a21130d8ee5b935f893c2cd0b63d7078e6a00cf7
AntoniaEkdahl <antoniaekdahl@gmail.com>
2023-07-18 12:45:52 +0200

    Add change name on component NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
