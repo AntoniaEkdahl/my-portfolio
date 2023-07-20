5f19071e3572ccd5c9427a1f3dbd808bd652e73b
AntoniaEkdahl <antoniaekdahl@gmail.com>
2023-07-20 15:07:11 +0200

    Add github links and netlify app to projekts

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,4 +1,5 @@
 import "./Work.css";
+import { Github } from "react-bootstrap-icons";
 
 function Work() {
   return (
@@ -9,30 +10,102 @@ function Work() {
         <div className="workContainer">
           Webshop React
           <div>2023 | Education | React</div>
+          <a
+            href="https://webshop-ae.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            See project
+          </a>
+          <a
+            href="https://github.com/AntoniaEkdahl/webshop-react"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Github />
+          </a>
         </div>
         <div className="workContainer">
           MineSweeper
           <div>2023 | Education | React</div>
+          <a
+            href="https://minesweeper-ae.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            See project
+          </a>
+          <a
+            href="https://github.com/AntoniaEkdahl/mineSweeper"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Github />
+          </a>
         </div>
         <div className="workContainer">
-          digital Clock
+          Digital Clock
           <div>2023 | Education | React</div>
+          <a
+            href="https://digital-clock-ae.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            See project
+          </a>
+          <a
+            href="https://github.com/AntoniaEkdahl/digital-clock"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Github />
+          </a>
         </div>
         <div className="workContainer">
           Whack-a-mole
           <div>2023 | Education | Angular</div>
+          <a href="" target="_blank" rel="noreferrer noopener">
+            See project
+          </a>
+          <a
+            href="https://github.com/AntoniaEkdahl/Whack-a-mole"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Github />
+          </a>
         </div>
         <div className="workContainer">
           BoringAPI
           <div>2023 | Education | Angular</div>
+          <a href="/" target="_blank" rel="noreferrer noopener">
+            See project
+          </a>
         </div>
         <div className="workContainer">
           Project StarWars
-          <div>2022 | Education | Vanilla JS</div>
+          <div>2022 | Education - group | Vanilla JS</div>
+          <a
+            href="https://group-project-starwars-encyclopedia.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            See project
+          </a>
+          <a
+            href="https://github.com/AntoniaEkdahl/Projekt-Starwars"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Github />
+          </a>
         </div>
         <div className="workContainer">
           Ajax Weather
           <div>2022 | Education | Vanilla JS</div>
+          <a href="/" target="_blank" rel="noreferrer noopener">
+            See project
+          </a>
         </div>
       </div>
     </>
