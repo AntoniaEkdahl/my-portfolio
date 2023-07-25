9100cd7a9aac2d16c8e747cb4bc7d2be9af447c1
AntoniaEkdahl <antoniaekdahl@gmail.com>
2023-07-25 13:18:59 +0200

    Fix all links to work for deploy sites and github

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -11,19 +11,20 @@ function Work() {
           Webshop React
           <div>2023 | Education | React</div>
           <a
+            className="projectLink"
             href="https://webshop-ae.netlify.app/"
             target="_blank"
             rel="noreferrer noopener"
           >
             See project
           </a>
+          <br />
           <a
+            className="projectLink"
             href="https://github.com/AntoniaEkdahl/webshop-react"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <br />
-
             <Github />
           </a>
         </div>
@@ -31,19 +32,20 @@ function Work() {
           MineSweeper
           <div>2023 | Education | React</div>
           <a
+            className="projectLink"
             href="https://minesweeper-ae.netlify.app/"
             target="_blank"
             rel="noreferrer noopener"
           >
             See project
           </a>
+          <br />
           <a
+            className="projectLink"
             href="https://github.com/AntoniaEkdahl/mineSweeper"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <br />
-
             <Github />
           </a>
         </div>
@@ -51,71 +53,125 @@ function Work() {
           Digital Clock
           <div>2023 | Education | React</div>
           <a
+            className="projectLink"
             href="https://digital-clock-ae.netlify.app/"
             target="_blank"
             rel="noreferrer noopener"
           >
             See project
           </a>
+          <br />
           <a
+            className="projectLink"
             href="https://github.com/AntoniaEkdahl/digital-clock"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <br />
             <Github />
           </a>
         </div>
         <div className="workContainer">
           Whack-a-mole
           <div>2023 | Education | Angular</div>
-          <a href="/" target="_blank" rel="noreferrer noopener">
+          <a
+            className="projectLink"
+            href="https://whack-a-mole-ae.vercel.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
             See project
           </a>
+          <br />
           <a
+            className="projectLink"
             href="https://github.com/AntoniaEkdahl/Whack-a-mole"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <br />
             <Github />
           </a>
         </div>
         <div className="workContainer">
-          BoringAPI
+          Boring-API
           <div>2023 | Education | Angular</div>
-          <a href="/" target="_blank" rel="noreferrer noopener">
+          <a
+            className="projectLink"
+            href="https://boring-api-ae.vercel.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
             See project
+          </a>
+          <br />
+          <a
+            className="projectLink"
+            href="https://github.com/AntoniaEkdahl/Boring-API/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Github />
           </a>
         </div>
         <div className="workContainer">
           Project StarWars
-          <div>2022 | Education - group | Vanilla JS</div>
+          <div>2022 | Education - group project | Vanilla JS</div>
           <a
+            className="projectLink"
             href="https://group-project-starwars-encyclopedia.netlify.app/"
             target="_blank"
             rel="noreferrer noopener"
           >
             See project
           </a>
+          <br />
           <a
+            className="projectLink"
             href="https://github.com/AntoniaEkdahl/Projekt-Starwars"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <br />
             <Github />
           </a>
         </div>
         <div className="workContainer">
-          Ajax Weather
+          Ajax Weather in Malmö
           <div>2022 | Education | Vanilla JS</div>
           <a
-            href="https://github.com/AntoniaEkdahl/JS1-Uppgift2"
+            className="projectLink"
+            href="https://weather-ajax-ae.vercel.app/"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <br />
+            See project
+          </a>
+          <br />
+          <a
+            className="projectLink"
+            href="https://github.com/AntoniaEkdahl/weather-ajax"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Github />
+          </a>
+        </div>
+        <div className="workContainer">
+          IKEA - replica
+          <div>2022 | Education | CSS </div>
+          <a
+            className="projectLink"
+            href="https://ikea-replica-ae.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            See project
+          </a>
+          <br />
+          <a
+            className="projectLink"
+            href="https://github.com/AntoniaEkdahl/IKEA-replica"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
             <Github />
           </a>
         </div>
