8b1a13fd05c7634cc6046c437b9dd02816dedb7e
AntoniaEkdahl <antoniaekdahl@gmail.com>
2023-07-30 09:05:35 +0200

    Add google fonts to all component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ function Home() {
   return (
     <>
       <div className="textContainer" id="home">
-        <h1 className="title">
+        <h1 className="title animate__fadeInUp">
           Hello,
           <br />
           my name is Antonia.
         </h1>
-        <div className="homeText">
+        <div className="homeText animate__fadeInLeft">
           I´m a Frontend Developer student based in Malmö, with an interest in
           finding creative solutions to develop visually appealing websites.
           <br />I love exploring and creating. I'm a lifelong learner.
